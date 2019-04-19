bf86981fd384a8d52be30ad0d9c89315f6170e4d
werbystonlemos <werbyston@hotmail.com>
2019-04-19 15:47:02 -0300

    finalizacao da tela de login

--- projetoFakeMSN/index.php
+++ projetoFakeMSN/index.php
@@ -3,11 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="img/logoMSN.png" />
         <!-- style do bootstrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <!-- style do projeto -->
         <link rel="stylesheet" href="main.css">
         <title>FakeWindows Live</title>
+        <script src="js/jquery-3.4.0.min.js" type="text/javascript"></script>
     </head>
     <body>
     <div class="col-sm-4 col-md-3 col-lg-3" id="containerPrincipal">
@@ -18,9 +20,8 @@
             </p>
         </div>
         <div id="ajudaOptions">
-            <div class="ajudaOptionsItens"><a href="#">Opção1</a></div>
-            <div class="ajudaOptionsItens"><a href="#">Opção1</a></div>
-            <div class="ajudaOptionsItens"><a href="#">Opção1</a></div>
+            <div class="ajudaOptionsItens"><a href="#">Dúvida</a></div>
+            <div class="ajudaOptionsItens"><a href="#">Cotate-nos</a></div>
         </div>
         
         <!-- perfil -->
@@ -30,28 +31,32 @@
 
         <!-- formulario e configs do no credenciamento -->
         <form id="formulario" name="formulario" method="post" action="class/controllerIndex.php">
-            <p>Endereço de email:</p>
+            <p for="email">Endereço de email:</p>
             <input id="email" required name="email" type="email"/>
-            <p>Senha:</p>
+            <p for="senha">Senha:</p>
             <input id="senha" required name="senha" type="password"/>
             
             <div id="entrarComo">
-                <p id="">Entrar como <span>Online</span><span id="dropSimbolo" class="glyphicon glyphicon-triangle-bottom" id="ajudaOpcao"></span></p>
+                <p id="">Entrar como <span id="statusCurrent">Invisível</span><span id="dropSimbolo" class="glyphicon glyphicon-triangle-bottom" id="ajudaOpcao"></span></p>
                 <!-- Status static mas tem que alterar para dinamico -->
-                <input type="hidden" id="status" name="status" value="Online" />
-            </div>
+                <input type="hidden" id="status" name="status" value="Invisível" />
+                <div id="statusOptions">
+                    <div class="statusOptionsItens"onclick="escolheStatus('Online')"><p>Online</p></div>
+                    <div class="statusOptionsItens"onclick="escolheStatus('Offline')"><p>Offline</p></div>
+                    <div class="statusOptionsItens"onclick="escolheStatus('Invisível')"><p>Invisível</p></div>
+                </div>
                 
-            <div class="lembretes">
-                <input id="lembra" class="" type="checkbox" name="lembra"/>
-                <p class="">Lembrar-me:</p>
-            </div>
-            
-            <div class="lembretes">
-                <input id="lembraSenha" class="" type="checkbox" name="lembraSenha"/>
-                <p>Lembrar minha senha:</p>
-            </div>
+                <div id="das" class="lembretes">
+                    <input id="lembra" class="" type="checkbox" name="lembra"/>
+                    <p class="">Lembrar-me:</p>
+                </div>
+                
+                <div class="lembretes">
+                    <input id="lembraSenha" class="" type="checkbox" name="lembraSenha"/>
+                    <p>Lembrar minha senha:</p>
+                </div>
 
-            <button id="" onclick="validaDados()" name="">Entrar</button>
+                <button id="" onclick="validaDados()" name="">Entrar</button>
 
         </form>
         
@@ -65,7 +70,7 @@
     <!-- JavaScript do projeto -->
     <script src="main.js"></script>
     <!-- jQuery primeiro e Bootstrap JS -->
-    <script src="js/jquery-3.4.0.min.js"></script>
+    <script src="js/jquery-3.4.0.min.js" type="text/javascript"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="js/bootstrap.min.js" ></script>
 </body>
--- projetoFakeMSN/main.js
+++ projetoFakeMSN/main.js
@@ -1,7 +1,38 @@
-$("#ajudaIcon").click(function(){ 
-    $("#ajudaOptions").toggle();
-});
 function validaDados()
 {
     document.formulario.subimit();
+}
+//document.getElementById("ajudaIcon").onclick = function() {alert("asffad")};
+
+$("#ajudaIcon").click(function(){
+    $("#ajudaOptions").toggle(400);
+});
+
+$("#dropSimbolo").click(function(){
+    $("#statusOptions").toggle();
+})
+
+function escolheStatus(status)
+{
+    let elementoInputPhp = document.getElementById("status");
+    let elemento = document.getElementById("statusCurrent");
+    switch(status)
+    {
+        case "Online":
+        elemento.style.color = "#20FD20";
+        elemento.style.fontWeight = "bolder";
+        break;
+        
+        case "Offline":
+        elemento.style.color = "grey";
+        break;
+        
+        case "Invisível":
+        elemento.style.color = "silver";
+        break;
+    }
+    
+    elemento.innerHTML = status;
+    elementoInputPhp.value = status;
+    $("#statusOptions").toggle();
 }
