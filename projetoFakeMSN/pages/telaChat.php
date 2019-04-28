2d605565ef7bdf05ba65fb8d65e3cc090dbc3699
werbystonlemos <werbyston@hotmail.com>
2019-04-28 15:52:34 -0300

    inicio da construcao da tela de chat

---
+++ projetoFakeMSN/pages/telaChat.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="../img/logoMSN.png" />
+    <!-- style do bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- style do projeto -->
+    <link rel="stylesheet" href="telaChat.css">
+    <title>FakeWindows Live</title>
+    <script src="../js/jquery-3.4.0.min.js" type="text/javascript"></script>
+</head>
+<body>
+    <section id="corpo">
+        <div class="container corpoDeDentro">
+            <div id="remetenet">
+                <div class="fotoPerfil">peffil</div>
+                <div class="areaConversa">conversa</div>
+            </div>
+            <div id="destinatario">
+                <div class="fotoPerfil">peffil</div>
+                <div class="areaConversa">conversa</div>
+            </div>
+        </div>
+    </section>
+
+    <!-- JavaScript do projeto -->
+    <script src="telaChat.js" type="text/javascript"></script>
+    <!-- jQuery primeiro e Bootstrap JS -->
+    <script src="../js/jquery-3.4.0.min.js" type="text/javascript"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="../js/bootstrap.min.js" ></script>
+</body>
+</html>
